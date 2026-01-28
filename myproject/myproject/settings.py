976c95b5b7ac49f1ea6273f91bf4fe4d825d0cac
ajubek <zyldyzbekalihanov915@gmail.com>
2026-01-28 17:25:57 +0600

    Insha Allah Byttym

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -94,14 +94,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lalafo_app',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'db',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
