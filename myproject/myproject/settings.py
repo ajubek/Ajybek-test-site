153b79b867f95c9aa05f91ebde088d841447ed18
ajubek <zyldyzbekalihanov915@gmail.com>
2026-01-28 17:13:31 +0600

    Insha Allah Byttym

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -149,6 +149,7 @@ MODELTRANSLATION_LANGUAGES = ('ky', 'en','ru','de')
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
